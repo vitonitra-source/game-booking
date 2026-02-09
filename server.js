d98fa2227c6edbf8a79fac4d49167a72e6e55942
vitonitra-source <vitonitra@gmail.com>
2026-02-09 20:11:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+origin: "*"
+}));
 app.use(express.json());
 
 /* ================= DATABASE ================= */
